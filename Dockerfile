9c9425c818d5d3e23b97b9c380a766835b795462
Parag Guruji <pguruji@bloomberg.net>
2023-10-05 20:14:06 -0400

    simplify using API instead of browser simulator

--- Dockerfile
+++ Dockerfile
@@ -1,53 +1,20 @@
 FROM python:3.11
 
 USER root
-RUN apt-get install -f
+RUN apt-get -f install
 RUN apt-get update
 RUN apt-get install -y virtualenv
 
-# RUN apt-get install -y chromium
-RUN wget http://archive.ubuntu.com/ubuntu/pool/main/libu/libu2f-host/libu2f-udev_1.1.4-1_all.deb
-RUN dpkg -i libu2f-udev_1.1.4-1_all.deb
-
-RUN apt-get install -f -y \
-    gconf-service  \
-    libasound2  \
-    libatk1.0-0  \
-    libcairo2  \
-    libcups2  \
-    libfontconfig1  \
-    libgdk-pixbuf2.0-0  \
-    libgtk-3-0  \
-    libnspr4  \
-    libpango-1.0-0  \
-    libxss1  \
-    fonts-liberation  \
-    libappindicator1  \
-    libnss3  \
-    lsb-release  \
-    xdg-utils \
-    libvulkan1
-
-RUN apt-get update
-RUN apt-get upgrade
-
-RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-RUN dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
-
-
 ENV APP_HOME /app
-WORKDIR $APP_HOME
-
 RUN virtualenv -p python3.11 $APP_HOME/venv
 ENV VIRTUAL_ENV $APP_HOME/venv
 ENV PATH $APP_HOME/venv/bin:$PATH
+WORKDIR $APP_HOME
 
 ADD requirements.txt $APP_HOME/requirements.txt
 RUN pip install -r $APP_HOME/requirements.txt
 
-COPY ./src $APP_HOME/src
 COPY ./resources $APP_HOME/resources
-
-USER cnb
+COPY ./src $APP_HOME/src
 
 ENTRYPOINT ["python", "-m", "src.main"]
--- src/main.py
+++ src/main.py
@@ -1,247 +1,107 @@
 import datetime
-import os
-import re
+import json
 import smtplib
 import ssl
 import sys
 import time
 from random import randint
-from types import TracebackType
-from typing import List, Optional, Dict, Callable, Tuple, Type
+from typing import List, Dict, Tuple, Any
 
-from bs4 import BeautifulSoup, element
-from selenium import webdriver
+import requests
 
-EMPTY_WEBPAGE = "<html><head></head><body></body></html>"
-
-PRICE_ADJUSTMENT = sum(v for v in dict(
-    DESTINATION_FEE=1390,
-    ORDER_FEE=250,
-    TITLE_FEE=60,
-    REGISTRATION_FEE=311,
-    FILING_FEE=12,
-    FEDERAL_INCENTIVE=-7500,
-    STATE_INCENTIVE=-1500,
-    REFERRAL=-500
-).values())
-
-WSGI_START_RESPONSE_TYPEDEF = Callable[
-    [str,
-     List[Tuple[str, str]],
-     Optional[tuple[Type[BaseException], BaseException, TracebackType] | tuple[None, None, None]]],
-    Callable[[bytes], None]
-]
-
-MAX_ATTEMPTS = 5
+from src.configuration import PRICE_ADJUSTMENT, URL_PARAMS, API_URL, MAX_ATTEMPTS, HEADERS, SENDER, RECIPIENTS, \
+    PASSWORD, BROWSER_URL, INTERVAL_SEC, WSGI_START_RESPONSE_TYPEDEF
 
 
 def backoff_random(_min=3, _max=9):
     time.sleep(randint(3, 9))
 
 
-class TeslaWatcher(object):
-    def __init__(self, url: str, timeout_sec: int, app_email: str, app_password: str, emails: List[str], limit: int):
-        self._url = url
-        self._timeout_sec = timeout_sec
-        self._app_email = app_email
-        self._app_password = app_password
-        self._target_emails = emails
-        self._limit = limit
-        chrome_options = webdriver.ChromeOptions()
-        # chrome_options.page_load_strategy = "normal"
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--disable-browser-side-navigation")
-        chrome_options.add_argument("--ignore-certificate-errors")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument(f"--user-agent=Mozilla/5.0 (X11; Linux x86_64) "
-                                    f"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36")
+def make_banner(from_email: str, to_emails: List[str], subject: str, content: List[str]) -> str:
+    header = [f"From: <{from_email}>", "To: " + ",".join([f"<{r}>" for r in to_emails]), subject]
+    width = (10 + max(map(len, header + content)))
+    return ("\n".join(
+        ["+" + "=" * width + "+"] +
+        ["|" + line + (" " * (width - len(line))) + "|" for line in header] +
+        ["+" + "-" * width + "+"] +
+        ["|" + line + (" " * (width - len(line))) + "|" for line in content] +
+        ["+" + "=" * width + "+"]
+    ))
 
-        chrome_options.add_argument("--disable-blink-features")
-        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        chrome_options.add_experimental_option('useAutomationExtension', False)
-        self.chrome_options = chrome_options
-        self._web_driver = None
 
-    def reset_webdriver(self):
-        # noinspection PyBroadException
+def notify(top_results: List[List[str]], total_results: int) -> str:
+    subject_line = "Subject: Tesla @ [{}]".format(datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
+    content_lines = ([f"Top {len(top_results)}/{total_results} matches from {BROWSER_URL}:", ""] +
+                     [line for result in top_results for line in result])
+    banner = make_banner(SENDER, RECIPIENTS, subject_line, content_lines)
+    email_message = "\n".join([subject_line] + content_lines)
+    if RECIPIENTS:
         try:
-            self._web_driver.quit()
-        except Exception:
-            pass
-        finally:
-            self._web_driver = None
-
-    def init_webdriver(self):
-        try:
-            self._web_driver = webdriver.Chrome(options=self.chrome_options)
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context()) as server:
+                server.login(SENDER, PASSWORD)
+                server.sendmail(SENDER, RECIPIENTS, email_message.encode("utf-8"))
         except Exception as e:
-            raise IOError(f"Error initializing webdriver for Chrome") from e
-
-    def fetch(self) -> str:
-        url = self._url
-        timeout_sec = self._timeout_sec
-        self.init_webdriver()
-        try:
-            self._web_driver.set_page_load_timeout(timeout_sec)
-            self._web_driver.get(url)
-            backoff_random()
-            page = self._web_driver.page_source
-            if EMPTY_WEBPAGE == page.strip():
-                raise IOError(f"Error fetching inventory: EMPTY RESPONSE: URL={url}")
-            return page
-        except Exception as e:
-            raise IOError(f"Error fetching inventory: URL={url}") from e
-        finally:
-            self.reset_webdriver()
-
-    def extract(self, html: Optional[str]) -> Tuple[List[List[str]], int]:
-        def parse_one(car_html: element.Tag) -> List[str]:
-            name = (
-                car_html
-                .select_one("section.result-header")
-                .select_one("div.result-basic-info")
-                .select_one("div.tds-text_color--10")
-                .text
-            )
-            price = (
-                car_html
-                .select_one("section.result-header")
-                .select_one("div.result-pricing")
-                .select_one("div.result-price")
-                .select_one("div.result-loan-payment")
-                .select_one("span.result-purchase-price")
-                .text
-            )
-            named_features = [
-                (item.select_one("div.tds-text--caption").select_one("span").text + ": " +
-                 item.select_one("div").select_one("span.tds-text--h4").text +
-                 item.select_one("div").find(name="span", attrs={"class": ""}).text
-                 )
-                for item in car_html
-                .select_one("section.result-highlights-cta")
-                .select_one("div.result-highlights")
-                .select_one("ul.highlights-list")
-                .select("li")
-            ]
-            unnamed_features = [_.text.replace("’", "'") for _ in (
-                car_html
-                .select_one("section.result-features.features-grid")
-                .select_one("ul.result-regular-features")
-                .select("li.tds-list-item")
-            )]
-            price = float(re.sub(r"[^0-9.]", "", price))
-            return (
-                [f"[${price:,.2f} => ${(price + PRICE_ADJUSTMENT):,.2f}]"] +
-                [f"  {v}" for v in [name] + named_features + unnamed_features] +
-                [""]
-            )
-
-        if html is None:
-            html = ""
-        url = self._url
-        limit = self._limit
-        try:
-            cars = (
-                BeautifulSoup(markup=html, features="html.parser")
-                .find(name="body")
-                .find(name="div", attrs={"id": "iso-container"})
-                .find(name="main")
-                .find(name="div", attrs={"class": "results-container"})
-                .find_all(name="article", attrs={"class": "result card"})
-            )
-        except Exception as e:
-            raise RuntimeError(f"Error parsing inventory: ContentLength={len(html)} URL={url} head={html[:500]}") from e
-        else:
-            results = []
-            found = len(cars)
-            for idx, car in enumerate(cars, start=1):
-                if idx > limit:
-                    break
-                try:
-                    results.append(parse_one(car))
-                except Exception as e:
-                    raise RuntimeError(f"Error parsing Car #{idx}/{len(cars)}: URL={url} HTML={car}") from e
-        return results, found
-
-    def notify(self, top_results: List[List[str]], total_results: int) -> str:
-
-        def make_banner(from_email: str, to_emails: List[str], subject: str, content: List[str]) -> str:
-            header = [f"From: <{from_email}>", "To: " + ",".join([f"<{r}>" for r in to_emails]), subject]
-            width = (10 + max(map(len, header + content)))
-            return ("\n".join(
-                ["+" + "=" * width + "+"] +
-                ["|" + line + (" " * (width - len(line))) + "|" for line in header] +
-                ["+" + "-" * width + "+"] +
-                ["|" + line + (" " * (width - len(line))) + "|" for line in content] +
-                ["+" + "=" * width + "+"]
-            ))
-
-        url = self._url
-        sender = self._app_email
-        password = self._app_password
-        recipients = self._target_emails
-        subject_line = "Subject: Tesla @ [{}]".format(datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
-        content_lines = (
-            [f"Top {len(top_results)}/{total_results} matches ({url}):", ""] +
-            [line for result in top_results for line in result]
-        )
-        banner = make_banner(sender, recipients, subject_line, content_lines)
-        email_message = "\n".join([subject_line] + content_lines)
-        if recipients:
-            try:
-                with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context()) as server:
-                    server.login(sender, password)
-                    server.sendmail(sender, recipients, email_message.encode("utf-8"))
-            except Exception as e:
-                raise IOError(f"Error notifying results: Results={banner}") from e
-        return banner
-
-    def run(self) -> str:
-        attempt = 0
-        while True:
-            attempt += 1
-            try:
-                return self.notify(*self.extract(self.fetch()))
-            except Exception as e:
-                if attempt < MAX_ATTEMPTS:
-                    print(f"Failed Attempt #{attempt}: Error={repr(e)}")
-                    backoff_random()
-                else:
-                    raise e
+            raise IOError(f"Error notifying results: Results={banner}") from e
+    return banner
 
 
-def local_main(tesla_watcher: TeslaWatcher, interval_sec: int):
-    done = False
+def extract(page: dict[str, Any]) -> Tuple[List[List[str]], int]:
+    total = page["total_matches_found"]
+    lines = []
+    for car in page["results"]:
+        price = car["InventoryPrice"]
+        car_lines = [
+            f"[${price:,.2f} => ${(price + PRICE_ADJUSTMENT):,.2f}]",
+            f'  {car["Year"]} Tesla {car["TrimName"]}'
+        ]
+        for spec in car["OptionCodeSpecs"]["C_SPECS"]["options"]:
+            car_lines.append(f'  {spec["description"]}: {spec["name"]}')
+        for opt in car["OptionCodeSpecs"]["C_OPTS"]["options"]:
+            car_lines.append(f'  {opt["name"]}')
+        lines.append(car_lines)
+    return lines, total
+
+
+def fetch() -> dict[str, Any]:
+    try:
+        resp = requests.get(API_URL, params=URL_PARAMS, headers=HEADERS, timeout=60)
+        if resp.status_code == 200:
+            return json.loads(resp.content)
+        raise IOError(f"ResponseCode={resp.status_code}")
+    except Exception as e:
+        pr = requests.Request(method="GET", url=API_URL, params=URL_PARAMS, headers=HEADERS).prepare()
+        raise IOError(f"Failed to fetch: URL={pr.url} headers={pr.headers}") from e
+
+
+def run():
+    attempt = 0
     while True:
-        if done:
-            time.sleep(interval_sec)
-        done = tesla_watcher.run()
+        attempt += 1
+        try:
+            return notify(*extract(fetch()))
+        except Exception as e:
+            if attempt < MAX_ATTEMPTS:
+                print(f"Failed Attempt #{attempt}: Error={repr(e)}")
+                backoff_random()
+            else:
+                raise e
+
+
+def local_main():
+    while True:
+        try:
+            print(run())
+        except Exception as e:
+            print(f"All attempts failed: Error={repr(e)}")
+            backoff_random()
+        backoff_random(INTERVAL_SEC, INTERVAL_SEC)
 
 
 def app(environ: Dict[str, str], start_response: WSGI_START_RESPONSE_TYPEDEF):
-    overridden_environ = os.environ.copy()
-    overridden_environ.update(environ)
-    _interval_sec = 60 * 60
-    _timeout_sec = 60
-    _url = "https://www.tesla.com/inventory/new/my?TRIM=LRAWD&arrangeby=plh&zip=07065&range=0"
-    _app_email = overridden_environ["SMTP_USER_EMAIL"]
-    _app_password = overridden_environ["SMTP_USER_PASSWORD"]
-    _target_emails = [email_id.strip() for email_id in open("resources/mailing_list.txt") if email_id.strip()]
-    _max_results = 3
-    _watcher = TeslaWatcher(
-        url=_url,
-        timeout_sec=_timeout_sec,
-        app_email=_app_email,
-        app_password=_app_password,
-        emails=_target_emails,
-        limit=_max_results
-    )
+    if environ:
+        print(f"ENVIRON:\n{json.dumps(environ, indent=4)}")
     status = "200 OK"
-    data = f"{_watcher.run()}"
+    data = f"{run()}"
     response_headers = [
         ("Content-type", "text/plain"),
         ("Content-Length", str(len(data)))
